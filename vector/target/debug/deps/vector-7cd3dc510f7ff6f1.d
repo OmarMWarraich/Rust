e79ef48ef6e43af0879569d313eccb1748556423
OmarMWarraich <earthling986@gmail.com>
2020-08-16 21:00:40 +0500

    vector_

---
+++ vector/target/debug/deps/vector-7cd3dc510f7ff6f1.d
@@ -0,0 +1,5 @@
+/home/remo/Desktop/vs/rust/vector/target/debug/deps/vector-7cd3dc510f7ff6f1: src/main.rs
+
+/home/remo/Desktop/vs/rust/vector/target/debug/deps/vector-7cd3dc510f7ff6f1.d: src/main.rs
+
+src/main.rs:
---
+++ vector/target/debug/vector.d
@@ -0,0 +1,1 @@
+/home/remo/Desktop/vs/rust/vector/target/debug/vector: /home/remo/Desktop/vs/rust/vector/src/main.rs
