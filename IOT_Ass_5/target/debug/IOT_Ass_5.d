e8428d6c21c01cc32092e00a4e1e34c429a7083f
OmarMWarraich <earthling986@gmail.com>
2020-08-16 20:59:34 +0500

    Ass_5_

---
+++ IOT_Ass_5/target/debug/IOT_Ass_5.d
@@ -0,0 +1,1 @@
+/home/remo/Desktop/vs/rust/IOT_Ass_5/target/debug/IOT_Ass_5: /home/remo/Desktop/vs/rust/IOT_Ass_5/src/main.rs
---
+++ IOT_Ass_5/target/debug/deps/IOT_Ass_5-c26b7eefcc7e1f87.d
@@ -0,0 +1,5 @@
+/home/remo/Desktop/vs/rust/IOT_Ass_5/target/debug/deps/IOT_Ass_5-c26b7eefcc7e1f87: src/main.rs
+
+/home/remo/Desktop/vs/rust/IOT_Ass_5/target/debug/deps/IOT_Ass_5-c26b7eefcc7e1f87.d: src/main.rs
+
+src/main.rs:
